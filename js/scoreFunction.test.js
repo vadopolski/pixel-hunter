c1f386fc3bad06473a4f1ffa91807aaffb1cad9e
vadopolski <vop444@gmail.com>
2018-06-14 22:29:41 +0300

    Refactoring

--- js/scoreFunction.test.js
+++ js/scoreFunction.test.js
@@ -38,18 +38,33 @@ describe(`#scoreInTheEnd()`, () => {
 
 describe(`#scoreInTheEnd()`, () => {
     it(`should return 1100 when the right answers equal 10 and time answer fast, slow and middle`, () => {
-        assert.equal(1100, scoreInTheEnd(answersWithSlowAndFast, remainingLives));
+        const expectedFastSlowMiddleValue = 1100;
+        const remainingLives = 10;
+
+        const actualFastSlowMiddleValue = scoreInTheEnd(answersWithSlowAndFast, remainingLives);
+
+        assert.equal(expectedFastSlowMiddleValue, actualFastSlowMiddleValue);
     });
 });
-//
-// describe(`#scoreInTheEnd()`, () => {
-//     it(`should return 1200 when the right answers equal 10 and time answer is fast and is not slow`, () => {
-//         assert.equal(1250, scoreInTheEnd(answersWithoutSlowAndWithFast, remainingLives));
-//     });
-// });
-//
-// describe(`#scoreInTheEnd()`, () => {
-//     it(`should return 1200 when the right answers equal 10 and time answer is fast, slow and middle`, () => {
-//         assert.equal(1000, scoreInTheEnd(answersWithSlowAndWithoutFast, remainingLives));
-//     });
-// });
+
+describe(`#scoreInTheEnd()`, () => {
+    it(`should return 1200 when the right answers equal 10 and time answer is fast and is not slow`, () => {
+        const expectedFastNotSlowValue = 1250;
+        const remainingLives = 10;
+
+        const actualFastNotSlowValue = scoreInTheEnd(answersWithoutSlowAndWithFast, remainingLives);
+
+        assert.equal(expectedFastNotSlowValue, actualFastNotSlowValue);
+    });
+});
+
+describe(`#scoreInTheEnd()`, () => {
+    it(`should return 1000 when the right answers equal 10 and time answer is fast, slow and middle`, () => {
+        const expectedFastSlowMiddleValue = 1000;
+        const remainingLives = 10;
+
+        const actualFastSlowMiddleValue = scoreInTheEnd(answersWithSlowAndWithoutFast, remainingLives);
+
+        assert.equal(expectedFastSlowMiddleValue, actualFastSlowMiddleValue);
+    });
+});
